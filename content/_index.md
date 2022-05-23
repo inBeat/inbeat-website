57c372c6b7853210f2539b02b8f8bd23c532d635
Pulkit Aggarwal <BashCloud@users.noreply.github.com>
2022-05-23 21:37:04 +0530

    Free toolkit mobile (#152)
    * Update:styles
    
    * Update: faq section
    
    * Update:features section
    
    * added: influencer marketing.md
    
    * close first accordian question
    
    * Update:feature section
    
    * Update:testimonial section
    
    * Update:whatwedo
    
    * Update:influencer-marketing
    
    * Fixed: edge to edge scroll for the brands widget
    
    * Autogenerated main.min.css files [with gulp]
    
    * ui fixes
    
    * Added:images
    
    * Fixed:what we do section padding
    
    * Added:progress bar
    
    * Fixed: SignUp Button & CTA Styles
    
    * Updated Demo Video Link
    
    * Fixed: Main CTA in video popup
    
    * Fixed: z-index of Video popupOverlay
    
    * WIP: Adding free-toolkit section in home-page
    
    * Fixed:switchers
    
    * Update:styling
    
    * Update:content
    
    * Update:testimonial circles
    
    * Update:min
    
    * Fixed: Spacing in the swicher widget
    
    * Fixed: Switcher widget for dynamic animation
    
    * WIP: smooth scroll in testimonials
    
    * Fixed: reverse scroll in testimonial
    
    * Fixed:scroll
    
    * Update:free-toolkit section
    
    * Fixed: switcher
    
    * Update:progress bar styling
    
    * Auto generated build files
    
    * fixed:ont size
    
    * ui:fixes
    
    * Remove:row margin mobile view
    
    * Update:width client card
    
    * Remove:inline styles
    
    * Fixed:switcher mobile padding
    
    * fixed:tabs padding
    
    * Seperate:testimonial +client sec
    
    * Update:switcher content
    
    * Update: testimonial content
    
    * wip
    
    * Update: card mobile with
    
    * Update: client card width
    
    * Update: hero subtitle
    
    * Update:content
    
    * Update:font
    
    * Fixed: width of clients & testimonials
    
    * Autogen build files
    
    * Fixed: Auto-gen build files
    
    * Seperate: influencer-marketing + Update:ui
    
    * fixed:right padding testmonial mobile
    
    * fixed:switcher top spacing mobile
    
    * Fixed:switchers active tab spacing
    
    * fixed:testmonial sec mobile
    
    * Update:features card mobile
    
    * Update:what we do sec mobile
    
    * Wip:switchers dsktop
    
    * Desktop ui fixes
    
    * ADDED: Auto-build files
    
    * Fixed: Freetoolkits edge to edge on mobile
    
    * Improved spacing on Free Toolkits mobile view

--- content/_index.md
+++ content/_index.md
@@ -70,6 +70,31 @@ whatwedo:
     cta:
       link: https://app.inbeat.co/get-started
       text: Get started — it's free
+toolkits:
+    - title: Instagram Engagement Calculator
+      text: Gain insights on influencers with our rapid Instagram engagement checker.
+      icon: freetoolkit-ig-engagement
+      url: engagement-rate-calculator-instagram
+    - title: Instagram Money Calculator
+      text: Evaluate the potential cost for different types of influencer collaborations.
+      icon: freetoolkit-ig-money
+      url: collaboration-cost-calculator-instagram
+    - title: TikTok Engagement Calculator
+      text: Gain insights on influencers with our rapid TikTok engagement checker.
+      icon: freetoolkit-tiktok-engagement
+      url: engagement-rate-calculator-tiktok
+    - title: Instagram Fake Follower Checker
+      text: Get an influencer’s audience audit & identify fake, dormant or suspect followers.
+      icon: freetoolkit-pulsepodcast
+      url: fake-follower-checker
+    - title: Top Instagram Influencers 2021
+      text: Find the top influencers in any niche, country & city with our Top Lists.
+      icon: freetoolkit-ig-topinfluencers
+      url: top-influencers/instagram
+    - title: Top TikTok Influencers 2021
+      text: Find the top influencers in any niche, country & city with our Top Lists.
+      icon: freetoolkit-tiktok-topinfluencers
+      url: top-influencers/tiktok
 # startnow:
 #   title: Get crackin'
 #   description: Start your influencer campaign today, with inBeat.
--- themes/inbeat/assets/js/main.js
+++ themes/inbeat/assets/js/main.js
@@ -108,6 +108,60 @@ function scrollAnchors(e, respond) {
     }, 100);
 }
 
+function testimonial() {
+        $('.slider').slick({
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          speed: 5000,
+          cssEase: 'linear',
+          autoplay: true,
+          autoplaySpeed: 0,
+          arrows: false,
+          dots: false,
+          pauseOnHover: false,
+          responsive: [{
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3
+            }
+          }, {
+            breakpoint: 520,
+            settings: {
+              slidesToShow: 3
+            }
+          }]
+        });
+    var dots = document.querySelectorAll('.testimonial_circle');
+    dots[0].classList.add('active')
+    dots.forEach(function(dot,index){
+        dot.addEventListener('click', function (e) {
+            dots.forEach(function(i){
+                if(i.classList.contains('active')){
+                    i.classList.remove('active');
+                }  
+            })
+            dot.classList.add('active');
+            scroll(document.querySelectorAll('.client-card')[index])
+        })
+    });
+    var scroll = function (el) {
+        var elLeft = el.offsetLeft + el.offsetWidth;
+        var elParentLeft = el.parentNode.offsetLeft + el.parentNode.offsetWidth;
+        if (elLeft >= elParentLeft + el.parentNode.scrollLeft) {
+            el.parentNode.scrollBy({
+                left: el.offsetLeft + el.parentNode.offsetLeft,
+                behavior: 'smooth',
+              })
+        } else if (elLeft <= el.parentNode.offsetLeft + el.parentNode.scrollLeft) {
+              el.parentNode.scrollTo({
+                top: 0,
+                left: el.offsetLeft - el.parentNode.offsetLeft,
+                behavior: 'smooth'
+              })
+        }
+      }
+}
+
 function pricing() {
     // Select pricing interval
     var schedule = document.getElementById('pricing-row');
@@ -329,7 +383,6 @@ function faq() {
     var acc = document.getElementsByClassName('accordion-heading');
     if(acc.length === 0){return}
     var currentActive = acc[0];
-
     var toggleAccordionState = function (accordion) {
         accordion.nextElementSibling.classList.toggle("active");
         accordion.lastElementChild.classList.toggle('open');
@@ -341,7 +394,7 @@ function faq() {
         accordion.classList.remove("new-padding");
     };
 
-    toggleAccordionState(currentActive);
+    // toggleAccordionState(currentActive);
 
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener('mousedown', function (e) {
@@ -357,13 +410,32 @@ function faq() {
 
 function banner(){
     var banner = document.getElementById('banner');
+    var text = banner.querySelector('.popupTrigger')
+    var text = banner.querySelector('.popupTrigger')
+    var close = banner.querySelector('#close')
     var closeBtn = document.getElementById('close');
     var hero = document.getElementById('hero');
-    if (getCookie('banner-hide')) {
-        banner.classList.add("hide-banner");
-        if(hero == null){return}hero.classList.remove('has-banner');
-        return;
-    }
+    var prevScrollpos = window.pageYOffset;
+    var height = banner.clientHeight 
+    console.log(height)
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    banner.style.height =  '28px';
+    text.classList.remove("hide");
+    close.style.top = '0'
+  } else {
+    banner.style.height = "0px"
+    text.classList.add("hide");
+    close.style.top = '-30px'
+  }
+  prevScrollpos = currentScrollPos;
+}
+    // if (getCookie('banner-hide')) {
+    //     banner.classList.add("hide-banner");
+    //     if(hero == null){return}hero.classList.remove('has-banner');
+    //     return;
+    // }
     banner.classList.remove("hide-banner");
     closeBtn.addEventListener('click', function (e) {
         e.preventDefault();
@@ -406,6 +478,8 @@ function articleProgressBar() {
   getScroll();
 }
 
+
+
 (function () {
   scrollTo();
   header();
@@ -416,5 +490,6 @@ function articleProgressBar() {
   popup();
   faq();
   banner();
+  testimonial();
   articleProgressBar();
 })();
